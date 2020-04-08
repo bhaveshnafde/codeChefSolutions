591973d7fc22a46dcffa8ea2d10c81331db70b55
bhaveshnafde <36959638+bhaveshnafde@users.noreply.github.com>
2020-04-08 15:59:16 +0530

    Update UNITGCD.java

--- April Long Challange/UNITGCD.java
+++ April Long Challange/UNITGCD.java
@@ -1,169 +1,203 @@
-// Java implementation of the approach
-class Coprime
-{
+import java.io.*;
+import java.util.*;
+import java.math.*;
 
-static int MAXN = 1000001;
+class Codechef{
 
-// Stores smallest prime factor for every number
-static int[] spf = new int[MAXN];
 
-// Hash to store prime factors count
-static int[] hash1 = new int[MAXN];
+		static int gcd(int a, int b)
+    {
+        // Everything divides 0
+        if (a == 0 || b == 0)
+            return 0;
 
-// Function to calculate SPF (Smallest Prime Factor)
-// for every number till MAXN
-static void sieve()
-{
-	spf[1] = 1;
-	for (int i = 2; i < MAXN; i++)
+        // base case
+        if (a == b)
+            return a;
 
-		// Marking smallest prime factor for every
-		// number to be itself
-		spf[i] = i;
+        // a is greater
+        if (a > b)
+            return gcd(a-b, b);
 
-	// Separately marking spf for every even
-	// number as 2
-	for (int i = 4; i < MAXN; i += 2)
-		spf[i] = 2;
-
-	// Checking if i is prime
-	for (int i = 3; i * i < MAXN; i++)
-	{
-
-		// Marking SPF for all numbers divisible by i
-		if (spf[i] == i)
-		{
-			for (int j = i * i; j < MAXN; j += i)
-
-				// Marking spf[j] if it is not
-				// previously marked
-				if (spf[j] == j)
-					spf[j] = i;
-		}
-	}
-}
-
-// Function to store the prime factors after dividing
-// by the smallest prime factor at every step
-static void getFactorization(int x)
-{
-	int temp;
-	while (x != 1)
-	{
-		temp = spf[x];
-		if (x % temp == 0)
-		{
-
-			// Storing the count of
-			// prime factors in hash
-			hash1[spf[x]]++;
-			x = x / spf[x];
-		}
-		while (x % temp == 0)
-			x = x / temp;
-	}
-}
-
-// Function that returns true if there are
-// no common prime factors between x
-// and other numbers of the array
-static boolean check(int x)
-{
-	int temp;
-	while (x != 1)
-	{
-		temp = spf[x];
-
-		// Checking whether it common
-		// prime factor with other numbers
-		if (x % temp == 0 && hash1[temp] > 1)
-			return false;
-		while (x % temp == 0)
-			x = x / temp;
-	}
-	return true;
-}
-
-// Function that returns true if there is
-// an element in the array which is coprime
-// with all the other elements of the array
-static boolean hasValidNum(int []arr, int n)
-{
-
-	// Using sieve for generating prime factors
-	sieve();
-
-	for (int i = 0; i < n; i++)
-		getFactorization(arr[i]);
-
-	// Checking the common prime factors
-	// with other numbers
-
-	for (int i = 0; i < n; i++)
-		if (check(arr[i])){
-      System.out.println(arr[i]);
-      //return true;
+        return gcd(a, b-a);
     }
 
-	return false;
-}
+		static boolean coprime(int a, int b) {
 
+			if ( gcd(a, b) == 1)	return true;
 
-/* arr[]  ---> Input Array
-data[] ---> Temporary array to store current combination
-start & end ---> Staring and Ending indexes in arr[]
-index  ---> Current index in data[]
-r ---> Size of a combination to be printed */
-static void combinationUtil(int arr[], int n, int r,
-                      int index, int data[], int i)
-{
-    // Current combination is ready to be printed,
-    // print it
-    if (index == r) {
-        for (int j = 0; j < r; j++)
-            System.out.print(data[j] + " ");
-        System.out.println("");
-        return;
+			else	return false;
+		}
+
+		public static void pair(ArrayList<Integer> arr, ArrayList<ArrayList<Integer>> x){
+			int n = arr.size();
+			if(n == 1){
+				x.add(arr);
+				return;
+			}
+			for (int i = 0; i < n; i++)
+			 {
+					 for (int j = i+1; j < n; j++)
+					 {
+
+							 if(!coprime(arr.get(i),arr.get(j)))	return;
+					 }
+			 }
+			 x.add(arr);
+		}
+
+    public static void unitgcd(int n) {
+			ArrayList<Integer> arrli = new ArrayList<Integer>();
+			for(int i = 1; i <= n; i++) {
+				arrli.add(i);
+			}
+			int opsize = (int)Math.pow(2, n);
+			ArrayList<Integer> arr = new ArrayList<Integer>();
+			ArrayList<ArrayList<Integer>> x = new ArrayList<ArrayList<Integer>>();
+			for (int counter = 1; counter < opsize; counter++)
+			{
+					arr.clear();
+					for (int j = 0; j < n; j++)
+					{
+							if (BigInteger.valueOf(counter).testBit(j))
+									arr.add(arrli.get(j));
+					}
+					pair(arr, x);
+			}
+			System.out.println(x);
+
     }
-    hasValidNum(arr, n)
-    // When no more elements are there to put in data[]
-    if (i >= n)
-        return;
 
-    // current is included, put next at next
-    // location
-    data[index] = arr[i];
-    combinationUtil(arr, n, r, index + 1,
-                           data, i + 1);
+    public static void main(String[] args) {
+      try{
+        InputReader in = new InputReader(System.in);
+        PrintWriter w = new PrintWriter(System.out);
+        int t = in.nextInt();
+        while (t-- > 0) {
+					int n = in.nextInt();
+					unitgcd(n);
+					//System.out.println(x);
+        }
+        w.close();
+      }catch(Exception e){
 
-    // current is excluded, replace it with
-    // next (Note that i+1 is passed, but
-    // index is not changed)
-    combinationUtil(arr, n, r, index, data, i + 1);
+      }
+    }
+
+    static class InputReader {
+
+        private final InputStream stream;
+        private final byte[] buf = new byte[8192];
+        private int curChar, snumChars;
+        private SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int snext() {
+            if (snumChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= snumChars) {
+                curChar = 0;
+                try {
+                    snumChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (snumChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = snext();
+            while (isSpaceChar(c)) {
+                c = snext();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = snext();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = snext();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public long nextLong() {
+            int c = snext();
+            while (isSpaceChar(c)) {
+                c = snext();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = snext();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = snext();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public int[] nextIntArray(int n) {
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nextInt();
+            }
+            return a;
+        }
+
+        public String readString() {
+            int c = snext();
+            while (isSpaceChar(c)) {
+                c = snext();
+            }
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = snext();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public String nextLine() {
+            int c = snext();
+            while (isSpaceChar(c))
+                c = snext();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = snext();
+            } while (!isEndOfLine(c));
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null)
+                return filter.isSpaceChar(c);
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        private boolean isEndOfLine(int c) {
+            return c == '\n' || c == '\r' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+        }
+    }
 }
-
-// The main function that prints all combinations
-// of size r in arr[] of size n. This function
-// mainly uses combinationUtil()
-static void printCombination(int arr[], int n, int r)
-{
-    // A temporary array to store all combination
-    // one by one
-    int data[] = new int[r];
-
-    // Print all combination using temprary
-    // array 'data[]'
-    combinationUtil(arr, n, r, 0, data, 0);
-}
-
-public static void main(String[] args)
-{
-    int arr[] = {1,2,3,4,5}; 
-    int r = 3;
-    int n = arr.length;
-    printCombination(arr, n, r);
-}
-
-}
-
-// This code is contributed by chandan_jnu

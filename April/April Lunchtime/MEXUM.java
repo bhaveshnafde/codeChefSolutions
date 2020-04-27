d65670cc6b1ab4e8a058772567c39044de9c1d03
bhaveshnafde <36959638+bhaveshnafde@users.noreply.github.com>
2020-04-27 14:29:58 +0530

    s

--- April/April Lunchtime/MEXUM.java
+++ April/April Lunchtime/MEXUM.java
@@ -1,9 +1,10 @@
 import java.io.*;
 import java.util.*;
 
- public class InputForCC{
+class Codechef{
 
     public static void main(String[] args) {
+      try{
         InputReader in = new InputReader(System.in);
         PrintWriter w = new PrintWriter(System.out);
 
@@ -13,29 +14,106 @@ import java.util.*;
             int n = in.nextInt();
             int arr[] = new int[n];
             arr = in.nextIntArray(n);
-            int res = powerSet(arr, n);
-            w.println(ans);
+            powerSet(arr, n);
+
         }
         w.close();
+      }catch(Exception e){}
     }
 
-    static ArrayList<Integer> arrli = new ArrayList<Integer>();
-
-    static ArrayList<Integer> powerSet(int arr[], int n)
+    static int segregate(int arr[], int size)
     {
-        arrli.clear();
+        int j = 0, i;
+        for (i = 0; i < size; i++) {
+            if (arr[i] <= 0) {
+                int temp;
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                // increment count of non-positive
+                // integers
+                j++;
+            }
+        }
+
+        return j;
+    }
+
+    /* Find the smallest positive missing
+       number in an array that contains
+       all positive integers */
+    static int findMissingPositive(int arr[], int size)
+    {
+        int i;
+
+        // Mark arr[i] as visited by making
+        // arr[arr[i] - 1] negative. Note that
+        // 1 is subtracted because index start
+        // from 0 and positive numbers start from 1
+        for (i = 0; i < size; i++) {
+            int x = Math.abs(arr[i]);
+            if (x - 1 < size && arr[x - 1] > 0)
+                arr[x - 1] = -arr[x - 1];
+        }
+
+        // Return the first index value at which
+        // is positive
+        for (i = 0; i < size; i++)
+            if (arr[i] > 0)
+                return i + 1; // 1 is added becuase indexes
+        // start from 0
+
+        return size + 1;
+    }
+
+    /* Find the smallest positive missing
+       number in an array that contains
+       both positive and negative integers */
+    static int findMissing(int arr[], int size)
+    {
+        // First separate positive and
+        // negative numbers
+        int shift = segregate(arr, size);
+        int arr2[] = new int[size - shift];
+        int j = 0;
+        for (int i = shift; i < size; i++) {
+            arr2[j] = arr[i];
+            j++;
+        }
+        // Shift the array and call
+        // findMissingPositive for
+        // positive part
+        return findMissingPositive(arr2, j);
+    }
+
+    static int missing = 0;
+
+    static void powerSet(int arr[], int n)
+    {
+        ArrayList<Integer> arrli = new ArrayList<Integer>();
+        missing =0;
         powerset(arr, arrli, 0);
-        System.out.println(arrli);
+        System.out.println(missing);
     }
 
     static void powerset(int arr[], ArrayList<Integer> arrli, int index)
     {
-        if(index == arr.length()){
-            arrli.add(arrli);
+        if(index == arr.length){
+            if(arrli.isEmpty()){
+              missing+=1;
+            }else{
+              int[] a = new int[arrli.size()];
+              for (int i =0; i < arrli.size(); i++)
+                  a[i] = arrli.get(i);
+              missing += findMissing(a, a.length);
+            }
+            System.out.println(arrli+" "+missing);
+
             return ;
         }
-        powerset(str, curr, index+1);
-        powerset(str, curr+str.charAt(index), index+1);
+        powerset(arr, arrli, index+1);
+        arrli.add(arr[index]);
+        powerset(arr, arrli, index+1);
     }
 
     static class InputReader {

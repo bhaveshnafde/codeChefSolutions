e8399319fd71ebca48c210dd7f10851d232d9c37
bhaveshnafde <36959638+bhaveshnafde@users.noreply.github.com>
2020-05-24 22:42:27 +0530

    s

---
+++ May/Cook Off/CHEFRECP.java
@@ -0,0 +1,180 @@
+import java.io.*;
+import java.util.*;
+
+class Codechef{
+
+    public static void main(String[] args) {
+      try{
+        InputReader in = new InputReader(System.in);
+        PrintWriter w = new PrintWriter(System.out);
+
+        int t = in.nextInt();
+
+        while (t-- > 0) {
+            int n = in.nextInt();
+            int arr[] = new int[n];
+            arr = in.nextIntArray(n);
+            if(check(arr, n)){
+              w.println("YES");
+            }else{
+              w.println("NO");
+            }
+        }
+        w.close();
+      }catch(Exception E){
+      }
+    }
+
+    static boolean check(int arr[], int n){
+      HashMap<Integer, Integer> dict = new HashMap<Integer, Integer>(n);
+      dict.put(arr[0], 1);
+      for(int i = 1; i < n; i++){
+        if(arr[i] != arr[i-1]){
+            if(dict.containsKey(arr[i])){
+              return false;
+            }else{
+              dict.put(arr[i], 1);
+            }
+        }else{
+          dict.put(arr[i], dict.get(arr[i]) + 1);
+        }
+      }
+
+      Collection<Integer> a = dict.values();
+      Integer[] ar = (Integer[])(a.toArray(new Integer[a.size()]));
+      if(areDistinct(ar))
+      return true;
+      else
+      return false;
+    }
+
+    public static boolean areDistinct(Integer arr[])
+    {
+        // Put all array elements in a HashSet
+        Set<Integer> s =
+           new HashSet<Integer>(Arrays.asList(arr));
+
+        // If all elements are distinct, size of
+        // HashSet should be same array.
+        return (s.size() == arr.length);
+    }
+
+
+
+    static class InputReader {
+
+        private final InputStream stream;
+        private final byte[] buf = new byte[8192];
+        private int curChar, snumChars;
+        private SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int snext() {
+            if (snumChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= snumChars) {
+                curChar = 0;
+                try {
+                    snumChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (snumChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = snext();
+            while (isSpaceChar(c)) {
+                c = snext();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = snext();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = snext();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public long nextLong() {
+            int c = snext();
+            while (isSpaceChar(c)) {
+                c = snext();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = snext();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = snext();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public int[] nextIntArray(int n) {
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nextInt();
+            }
+            return a;
+        }
+
+        public String readString() {
+            int c = snext();
+            while (isSpaceChar(c)) {
+                c = snext();
+            }
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = snext();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public String nextLine() {
+            int c = snext();
+            while (isSpaceChar(c))
+                c = snext();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = snext();
+            } while (!isEndOfLine(c));
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null)
+                return filter.isSpaceChar(c);
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        private boolean isEndOfLine(int c) {
+            return c == '\n' || c == '\r' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+        }
+    }
+}
